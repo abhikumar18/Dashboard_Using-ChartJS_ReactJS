813a59983656e7ce401fd804960fdba17f922af0
abhikumar18 <abhishek.it.msit@gmail.com>
2023-10-28 01:05:02 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import BarChart from './components/BarChart';
+import { UserData } from './Data';
 function App() {
+ 
+  const [userData,setUserData] = useState({
+    labels: userData.map((data)=>data.year),
+    datasets:[{
+      label:"Users Gained",
+      data:userData.map((data)=>data.userGain)
+    }]
+  });
+
   return (
     <div className="App">
-      
+      <BarChart chartData={UserData}/>
     </div>
   );
 }
