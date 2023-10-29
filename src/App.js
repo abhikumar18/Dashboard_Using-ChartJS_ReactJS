3cf7858b55a13f99cad8f60c7c6ff6bca1919803
abhikumar18 <abhishek.it.msit@gmail.com>
2023-10-29 09:48:29 +0000

    More charts added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import { useState } from 'react';
 import './App.css';
 import BarChart from './components/BarChart';
 import { UserData } from './Data';
+import LineChart from './components/LineChart';
+import PieChart from './components/PieChart';
 function App() {
  
   const [userData,setUserData] = useState({
-    labels: userData.map((data)=>data.year),
+    labels: UserData.map((data)=>data.year),
     datasets:[{
       label:"Users Gained",
-      data:userData.map((data)=>data.userGain)
+      data:UserData.map((data)=>data.userGain),
+      backgroundColor:["blue","red"]
     }]
   });
 
   return (
     <div className="App">
-      <BarChart chartData={UserData}/>
+      <div style ={{width:700}}>
+        <BarChart chartData={userData}/>
+      </div>
+
+      <div style ={{width:700}}>
+        <LineChart chartData={userData}/>
+      </div>
+
+      <div style ={{width:700}}>
+        <PieChart chartData={userData}/>
+      </div>
     </div>
   );
 }
